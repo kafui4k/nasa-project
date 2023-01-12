41678995fab4f66dec2f49df92c91d889a9ad5d2
Kafui Alordo <kalordo7@gmail.com>
2023-01-12 16:49:33 +0000

    add a launches model

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,11 @@
+const path = require("path");
 const express = require("express");
+
 const cors = require("cors");
+const morgan = require("morgan");
+
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
@@ -9,7 +14,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
