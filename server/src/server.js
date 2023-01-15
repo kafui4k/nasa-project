71a42b0faa8eca0dfa3a9e71df92f23e6a3cab76
Kafui Alordo <kalordo7@gmail.com>
2023-01-15 00:05:15 +0000

    connect application to MongoDB database via mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,31 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://nasa-api:8BjHLVFcpxwbzDfF@nasacluster.yto0bbx.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+mongoose.set("strictQuery", false);
+
 async function startServer() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
